a106772389c0d97e0ec829cebfba8e65b5224615
sandy <sandyambarita@gmail.com>
2021-06-26 00:05:32 +0700

    Initial Commit

---
+++ Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Sandy Ambarita on 26/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
